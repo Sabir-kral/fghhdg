3788845ba3ee5f21dc2a173185f5140ca76b88f9
Sabir-kral <sabirmemmedli21152014@gmail.com>
2024-12-03 16:33:17 +0400

    dfgssdf

---
+++ app.js
@@ -0,0 +1,12 @@
+function ss() {
+    let ss = document.getElementById("ss")
+    ss.innerText = " "
+    let sd = document.getElementById("sd")
+    sd.innerText = "sagol"
+}
+function sd() {
+    let sd = document.getElementById("sd")
+    sd.innerText = " "
+    let ss = document.getElementById("ss")
+    ss.innerText = "salam"
+}
